f7443b3b015e96812c4aca5ab6694c344426d2df
diez37 <diezinthead@gmail.com>
2018-07-12 19:41:02 +0300

    rename index.js to request-block.js and init class

---
+++ request-block.js
@@ -0,0 +1,21 @@
+'use strict';
+
+export class RequestBlock {
+
+  /**
+   * @param {Object} config
+   *
+   * @returns {RequestBlock}
+   */
+  static configure(config) {
+    return RequestBlock;
+  }
+
+  /**
+   * @returns {RequestBlock}
+   */
+  static run() {
+    return RequestBlock;
+  }
+
+}
